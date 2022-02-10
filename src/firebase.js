9eb0fde3c8609215d26f8bd289d1af178024d94d
Nicole de Guzman <nicoledeguz@hotmail.com>
2022-02-10 12:12:35 -0500

    updated

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,37 @@
-import firebase from "firebase/app";
+// import firebase from "firebase/app";
+// import "firebase/database";
+// Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDGGGb95TwGTY8m5p1jAfSmAOZ5Y1iN3SE",
+//   authDomain: "juno-zoltan.firebaseapp.com",
+//   databaseURL: "https://juno-zoltan-default-rtdb.firebaseio.com",
+//   projectId: "juno-zoltan",
+//   storageBucket: "juno-zoltan.appspot.com",
+//   messagingSenderId: "232035626294",
+//   appId: "1:232035626294:web:7e942bf960397c0bddd797"
+// };
+// Initialize Firebase
+// firebase.initializeApp(firebaseConfig);
+// export default firebase;
+
+import firebase from 'firebase/compat/app';
 import "firebase/database";
+// Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBaCIzc0d5Z1CpNMsVZC7UJG7EWkgHJy_g",
-  authDomain: "zoltannat-55eaa.firebaseapp.com",
-  databaseURL: "https://zoltannat-55eaa-default-rtdb.firebaseio.com",
-  projectId: "zoltannat-55eaa",
-  storageBucket: "zoltannat-55eaa.appspot.com",
-  messagingSenderId: "760219817103",
-  appId: "1:760219817103:web:d1eef7a571873c20420cfa"
+  apiKey: "AIzaSyDZ7hp5FATGh4QL45xF9r-GlKrb9meBFm0",
+  authDomain: "nygma-machine.firebaseapp.com",
+  projectId: "nygma-machine",
+  storageBucket: "nygma-machine.appspot.com",
+  messagingSenderId: "692492057651",
+  appId: "1:692492057651:web:d5e9b90cbd1198efccf746"
 };
+
 // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
 firebase.initializeApp(firebaseConfig);
 export default firebase;
